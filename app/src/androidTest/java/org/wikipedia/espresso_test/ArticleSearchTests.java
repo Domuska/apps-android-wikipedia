3dbdc2488106c02647f206fd7fd01b42aa6cdfea
Tomi Lamsa <lamsatom@gmail.com>
2016-07-22 13:18:38 +0300

    added start of Utils class, added teardown to ArticleSearchTests, added required database clear method to Database.java

--- app/src/androidTest/java/org/wikipedia/espresso_test/ArticleSearchTests.java
+++ app/src/androidTest/java/org/wikipedia/espresso_test/ArticleSearchTests.java
@@ -9,50 +9,35 @@ import org.junit.runner.RunWith;
 import org.wikipedia.MainActivity;
 import org.wikipedia.R;
 import org.wikipedia.TestingHelpers.SearchIdlingResource;
-import org.wikipedia.search.SearchResult;
+import org.wikipedia.database.Database;
+import org.wikipedia.espresso_test.Utilities.Utils;
 
-import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.Espresso;
-import android.support.test.espresso.action.TypeTextAction;
-import android.support.test.espresso.matcher.ViewMatchers;
-import android.support.test.espresso.web.webdriver.Locator;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.widget.EditText;
+import android.support.v7.preference.PreferenceManager;
 
-import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static android.support.test.espresso.web.assertion.WebViewAssertions.webContent;
 import static android.support.test.espresso.web.assertion.WebViewAssertions.webMatches;
-import static android.support.test.espresso.web.matcher.DomMatchers.hasElementWithId;
 import static android.support.test.espresso.web.sugar.Web.onWebView;
-import static android.support.test.espresso.web.webdriver.DriverAtoms.findElement;
-import static android.support.test.espresso.web.webdriver.DriverAtoms.webClick;
-import static junit.framework.Assert.assertEquals;
 import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.endsWith;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasEntry;
-import static org.hamcrest.Matchers.hasToString;
-import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
+
 
 @RunWith(AndroidJUnit4.class)
 public class ArticleSearchTests {
 
+    private String articleName = "Final Fantasy XII";
+
     @Rule
     public ActivityTestRule<MainActivity> myActivityRule =
             new ActivityTestRule<MainActivity>(MainActivity.class) {
-//        @Override
-//        protected void afterActivityLaunched() {
-//            // Enable JS!
-//            onWebView().forceJavascriptEnabled();
-//        }
     };
 
     @Before
@@ -63,60 +48,39 @@ public class ArticleSearchTests {
     @After
     public void tearDown(){
         Espresso.unregisterIdlingResources(SearchIdlingResource.getIdlingResource());
+
+        PreferenceManager.
+                getDefaultSharedPreferences(
+                        myActivityRule.getActivity().getApplicationContext())
+                .edit().clear().commit();
+
+        Database.clearDatabase(myActivityRule.getActivity().getApplicationContext());
     }
 
     @Test
-    public void testStuff(){
+    public void testSearchArticle_checkTitleShown(){
 
-        final String stringToBeTyped = "Final Fantasy XII";
-        String searchBarHint = myActivityRule.getActivity().getString(R.string.search_hint);
-        onView(allOf(withText(searchBarHint), withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE))).perform(click());
+        //open the article
+        Utils.searchAndOpenArticleWith(articleName, myActivityRule.getActivity());
 
-        //have to do a drity trick like this since otherwise IdlingResource will stop after each letter
-        //to wait for response to network call
-        final EditText searchField = (EditText) myActivityRule.getActivity().findViewById(R.id.search_src_text);
-        InstrumentationRegistry.getInstrumentation().runOnMainSync(new Runnable() {
-            @Override
-            public void run() {
-                searchField.setText(stringToBeTyped);
-            }
-        });
+        //check the title is displayed in the title view
+        onView(allOf(withId(R.id.view_article_header_text), withText(containsString(articleName))))
+                .check(matches(isDisplayed()));
 
+//        onWebView().forceJavascriptEnabled();
+//
+//        onWebView()
+//                .check(webContent(containingTextInBody(articleName)));
 
-//        Espresso.closeSoftKeyboard();
-//        onView(withText(stringToBeTyped)).perform(click());
+//        onWebView()
+//                .check(webMatches(getCurrentUrl(), is(articleName)));
 
-//        onData(hasEntry(equalTo("Data"), is("Final_Fantasy_XII")))
-//                .inAdapterView(withId(R.id.search_results_list))
-//                .perform(click());
-
-        //workses, precious.
-        onData(allOf(instanceOf(SearchResult.class), hasToString(is("Final_Fantasy_XII"))))
-                .inAdapterView(withId(R.id.search_results_list))
-                .perform(click());
-
-        onWebView().forceJavascriptEnabled();
 
 //        onView(withId(R.id.floating_toc_button)).perform(click());
 
-        //kirjota johonkin siitä että tämä ei anna kovin kummosta errorilogia jos ID:llä ei löydy mitään
-        onWebView()
-                .check(webContent(hasElementWithId("content_block_19")));
-
-
-
 //        onWebView()
-//                .withElement(findElement(Locator.XPATH, "//*[@id=\"content_block_19\"]"))
-//                .perform(webClick());
-
-//        onWebView()
-//                .withElement(findElement(Locator.ID, "content_block_19"))
-//                .check(webMatches())
-
-
-
-        //  //*[@id="content_block_19"]
-
-
+//                .check(webContent(hasElementWithId("content_block_19")));
     }
+
+    
 }
---
+++ app/src/androidTest/java/org/wikipedia/espresso_test/Utilities/Utils.java
@@ -0,0 +1,43 @@
+package org.wikipedia.espresso_test.Utilities;
+
+import android.app.Activity;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.widget.EditText;
+
+import org.wikipedia.R;
+import org.wikipedia.search.SearchResult;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.hasToString;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+
+public class Utils {
+
+    public static void searchAndOpenArticleWith(final String name, Activity currentActivity){
+        String searchBarHint = currentActivity.getString(R.string.search_hint);
+        onView(allOf(withText(searchBarHint), withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE))).perform(click());
+
+        //have to do a drity trick like this since otherwise IdlingResource will stop after each letter
+        //to wait for response to network call
+        final EditText searchField = (EditText) currentActivity.findViewById(R.id.search_src_text);
+        InstrumentationRegistry.getInstrumentation().runOnMainSync(new Runnable() {
+            @Override
+            public void run() {
+                searchField.setText(name);
+            }
+        });
+
+        //open the article in results
+        onData(allOf(instanceOf(SearchResult.class), hasToString(is("Final_Fantasy_XII"))))
+                .inAdapterView(withId(R.id.search_results_list))
+                .perform(click());
+    }
+}
