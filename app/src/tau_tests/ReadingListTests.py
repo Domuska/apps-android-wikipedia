8392a7d7173950b9b54460ca116192694077acce
Tomi Lamsa <lamsatom@gmail.com>
2016-08-01 14:18:53 +0300

    added readingListTest

---
+++ app/src/tau_tests/ReadingListTests.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from Utilities import TestDataSource, Utils
+class ReadingListTests (UITestCase):
+
+    def setUp(self):
+        global articleName1, readingListName, gotItText, readingListText
+        
+        articleName1 = TestDataSource.articleName1
+        readingListName = TestDataSource.readingListName
+        gotItText = "Got it"
+        readingListText = "Reading lists"
+        
+        launch.activity('org.wikipedia.alpha', 'org.wikipedia.MainActivity', verify=False)
+                    
+    def tearDown(self):
+        packages.clearData('org.wikipedia.alpha')
+
+    @testCaseInfo('<add article to reading list>', deviceCount=1)
+    def testAddArticleToReadingList(self):
+        
+        log("open an article")
+        Utils.openSearchFromStartScreen();
+        Utils.searchAndOpenArticleWithName(articleName1)
+        
+        log("add it to reading list")
+        tap.resourceId('org.wikipedia.alpha:id/view_article_menu_bar_bookmark')
+        tap.text(gotItText)
+        input.text(readingListName)
+        tap.resourceId('android:id/button1')
+        
+        log("open reading lists and assert article is visible in the list")
+        Utils.openDrawer()
+        tap.text(readingListText)
+        
+        
+        tap.text(readingListName)
+        assert exists.text(articleName1),\
+        "article: " + articleName1 + " is not visible"
+
+        log("open the article and assert title is correct")
+        tap.text(articleName1)
+        Utils.assertArticleTitleContains(articleName1)
+
+        
