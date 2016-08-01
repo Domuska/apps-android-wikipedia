4d6cbf0947b99c57a0466a09a89f5c45d80764f3
Tomi Lamsa <lamsatom@gmail.com>
2016-08-01 10:28:38 +0300

    added check recent searches test

--- app/src/tau_tests/ArticleSearchTests.py
+++ app/src/tau_tests/ArticleSearchTests.py
@@ -4,8 +4,11 @@ from Utilities import TestDataSource, Utils
 class ArticleSearchTests (UITestCase):
     
     def setUp(self):
-        global articleName1
+        global articleName1, articleName2, articleName3, recentSearchesText
         articleName1 = TestDataSource.articleName1
+        articleName2 = TestDataSource.articleName2
+        articleName3 = TestDataSource.articleName3
+        recentSearchesText = "Recent searches:"
         
         launch.activity('org.wikipedia.alpha', 'org.wikipedia.MainActivity', verify=False)
         
@@ -21,4 +24,31 @@ class ArticleSearchTests (UITestCase):
         
         #check the title is displayed in the title view
         Utils.assertArticleTitleContains(articleName1)
+        
+        
+    @testCaseInfo('<Check recents>', deviceCount=1)
+    def testSearchArticle_checkRecentsShown(self):
+        
+        log("open some articles")
+        Utils.openSearchFromStartScreen()
+        Utils.searchAndOpenArticleWithName(articleName1)
+        
+        Utils.openSearchFromArticle()
+        Utils.searchAndOpenArticleWithName(articleName2)
+        
+        Utils.openSearchFromArticle()
+        Utils.searchAndOpenArticleWithName(articleName3)
+        
+        log("go to the search screen")
+        Utils.openSearchFromArticle()
+        tap.resourceId("org.wikipedia.alpha:id/search_close_btn")
+        assert exists.text(recentSearchesText)
+        
+        log("assert that the recent searches show the articles searched")
+        assert exists.text(articleName1)
+        assert exists.text(articleName2)
+        assert exists.text(articleName3)
+        
+        
+        
         
--- app/src/tau_tests/Utilities/Utils.py
+++ app/src/tau_tests/Utilities/Utils.py
@@ -3,9 +3,17 @@ def openSearchFromStartScreen():
 	tap.resourceId("org.wikipedia.alpha:id/search_container")
 	
 def searchAndOpenArticleWithName(articleName):
+	log("searching article with name " + articleName)
 	input.text(articleName)
+	log("opening the article")
 	tap.text(articleName, resourceId="org.wikipedia.alpha:id/page_list_item_title")
 	
 def assertArticleTitleContains(articleName):
 	titleView = get.item.resourceId("org.wikipedia.alpha:id/view_article_header_text")
 	assert articleName in titleView.Text, articleName + " not in title"
+	
+	
+def openSearchFromArticle():
+	log("opening search from article")
+	tap.resourceId("org.wikipedia.alpha:id/main_search_bar_text")
+	
