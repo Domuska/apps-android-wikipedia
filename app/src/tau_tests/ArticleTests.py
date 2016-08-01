1a7e5aa61377c983711e3fb4c4e4bd39fed387e8
Tomi Lamsa <lamsatom@gmail.com>
2016-08-01 13:00:11 +0300

    worked on table of contents tests - not working. Seems Tau can't handle webviews

--- app/src/tau_tests/ArticleTests.py
+++ app/src/tau_tests/ArticleTests.py
@@ -4,12 +4,14 @@ from Utilities import TestDataSource, Utils
 class ArticleTests (UITestCase):
     
     def setUp(self):
-        global articleName1, subHeading1, subHeading2, subHeading3
+        global articleName1, subHeading1, subHeading2, subHeading3, \
+        article1_referenceSubHeading 
         
         articleName1 = TestDataSource.articleName1
         subHeading1 = TestDataSource.article1_subheading1
         subHeading2 = TestDataSource.article1_subheading2
         subHeading3 = TestDataSource.article1_subheading3
+        article1_referenceSubHeading = TestDataSource.article1_referenceSubHeading
         
         launch.activity('org.wikipedia.alpha', 'org.wikipedia.MainActivity', verify=False)
             
@@ -31,14 +33,30 @@ class ArticleTests (UITestCase):
         
         log("make sure those same three subtitles are visible in the webview")
         Utils.closeToC()
-        assert find.text(subHeading1), \
+        
+        assert find.description(subHeading1), \
         "subheading: " + subHeading1 + " not found"
 
-        assert find.text(subHeading2), \
+        assert find.resourceId(subHeading2), \
         "subheading: " + subHeading2 + " not found"
 
-        assert find.text(subHeading3), \
-        "subheading: " + subHeading3 + " not found"         
+        assert find.resourceId(subHeading3), \
+        "subheading: " + subHeading3 + " not found"   
+        
+        
+    @testCaseInfo('<click subheading, see screen moved>', deviceCount=1)
+    def testScrollingToC_clickSubHeading(self):
+        
+        log("open article and table of contents")
+        Utils.openSearchFromStartScreen()
+        Utils.searchAndOpenArticleWithName(articleName1)
+        Utils.openToC()
+        
+        tap.text(article1_referenceSubHeading, area = "org.wikipedia.alpha:id/page_toc_list")
+        
+        assert exists.text(article1_referenceSubHeading), \
+        "subheading: " + article1_referenceSubHeading + " not visible on screen"
+        
         
         
         
