2cd609340c8539821f31bc448cef6b79a749da87
Tomi Lamsa <lamsatom@gmail.com>
2016-08-02 13:09:37 +0300

    Utils was left out

---
+++ app/src/androidTest/java/org/wikipedia/uiautomator_tests/Utils/TestDataSource.java
@@ -0,0 +1,27 @@
+package org.wikipedia.uiautomator_tests.Utils;
+
+public class TestDataSource {
+
+    public static String fullLinkText1 = "artificial intelligence";
+    public static String partialLinkText = "artificial";
+    public static String newArticleTitle = "";
+    public static String articleName1 = "Final Fantasy XII";
+    public static String articleName2 = "Google";
+    public static String articleName3 = "Scotland";
+    public static String articleName3_finnish = "Skotlanti";
+
+    public static String finnishLanguage = "Suomi";
+
+    public static String referencesElementId = "content_block_19";
+    public static String fullLinkTextCapitalized = "Artificial intelligence (video games)";
+    public static String readingListName = "Fancy stuff";
+    public static String article1_referenceSubHeading = "References";
+    public static String article1_firstSubHeading = "Gameplay";
+    public static String newTabDefaultText = "Main Page";
+
+    public static String article1_subheading1 = "Gameplay";
+    public static String article1_subheading2 =  "Plot";
+    public static String article1_subheading3 = "Development";
+    public static String article1_firstReference = "Gantayat, Anoop";
+    public static String referencesJSClassName = "app_table_collapsed_caption";
+}
