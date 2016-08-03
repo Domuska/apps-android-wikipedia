bf54e28515a44b0529f1fea26886269ad9834c78
Tomi Lamsa <lamsatom@gmail.com>
2016-08-03 15:41:28 +0300

    removed commented code

--- app/src/androidTest/java/org/wikipedia/uiautomator_tests/TabTests.java
+++ app/src/androidTest/java/org/wikipedia/uiautomator_tests/TabTests.java
@@ -46,7 +46,6 @@ public class TabTests  extends BaseTestClass{
         device.wait(Until.findObject(
                 By.text(newTabDefaultText)), GENERAL_TIMEOUT)
                 .click();
-//        device.findObject(By.text(newTabDefaultText)).click();
 
         //navigate to another article
         Utils.openSearchFromArticle(device);
@@ -63,18 +62,8 @@ public class TabTests  extends BaseTestClass{
 
     @Test
     public void testOpenArticleInNewTab(){
-
-        //        int headerHeight = device.findObject(
-//                By.res("org.wikipedia.alpha:id/view_article_header_text")).
-//        Point startPoint = new Point(device.getDisplayWidth()/2, device.getDisplayHeight()/5);
-//        Double endY = device.getDisplayHeight()*0.8;
-//        Point endPoint = new Point(startPoint.x, endY.intValue());
-
-//        device.drag(startPoint.x, startPoint.y, endPoint.x, endPoint.y, 4);
-//        device.drag(endPoint.x, endPoint.y, startPoint.x, startPoint.y, 5);
-
         //tällä tavalla linkin klikkaaminen toimii kyllä, en tiedä miksi mutta se toimii.
-        //uiautomatorviewerin dumpissa kyllä näkyy elementit näin. Voisiko sivua scrollata...?
+        //uiautomatorviewerin dumpissa kyllä näkyy elementit näin.
 
         //open one article
         Utils.openSearchFromStartScreen(device);
@@ -96,7 +85,6 @@ public class TabTests  extends BaseTestClass{
         device.wait(Until.findObject(
                 By.text(newArticleNameCapitalized)), GENERAL_TIMEOUT)
                 .click();
-//        device.findObject(By.text(newArticleNameCapitalized)).click();
 
         //assert title contains right text
         Utils.assertArticleTitleContains(device, newArticleNameCapitalized);
